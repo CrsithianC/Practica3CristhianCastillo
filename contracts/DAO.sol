21c897e0500c549f4676f14e891cdb4f527c9a0b
Cristhian Castillo <cristhiancastilloarcila@gmail.com>
2025-02-23 16:41:51 +0100

    Completar la DAO

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -55,20 +55,20 @@ contract KeepCodingDAO is Proposal, Counter {
         Comprueba si la propuesta existe.
         Si no existe lanza el error ProposalDoesNotExist.
      */
-     modifier doesProposalExist(uint256 _proposalId){
-        if(){
-            revert ;
+     modifier doesProposalExist(uint256 _proposalId) {
+        if (proposals[_proposalId].creator == address(0)) {
+            revert ProposalDoesNotExist(_proposalId);
         }
         _;
-     }
+    }
 
     /**
         Comprueba si la propuesta esta activa.
         Si no esta activa lanza el error ProposalDeadlineExceeded.
      */
-     modifier isProposalActive(uint256 _proposalId){
-        if(){
-            revert;
+     modifier isProposalActive(uint256 _proposalId) {
+        if (block.timestamp > proposals[_proposalId].deadline) {
+            revert ProposalDeadlineExceeded(_proposalId);
         }
         _;
     }
@@ -77,9 +77,9 @@ contract KeepCodingDAO is Proposal, Counter {
         Comprueba si la propuesta ha superado su deadline y puede ser ejecutada.
         Si no lo ha superado lanza el error ProposalCannotBeExecuted.
      */
-    modifier canProposalBeExecuted(uint256 _proposalId){
-        if(){
-            revert ;
+    modifier canProposalBeExecuted(uint256 _proposalId) {
+        if (block.timestamp <= proposals[_proposalId].deadline) {
+            revert ProposalCannotBeExecuted(_proposalId);
         }
         _;
     }
@@ -105,33 +105,24 @@ contract KeepCodingDAO is Proposal, Counter {
         Crea una propuesta e inicializa sus datos
      */
     function createProposal(string memory _title, string memory _description, string[] memory _options) public{
-        //obtener el id
-        
-        //crear el objeto proposal
-        
-        //rellenar el id
-        
-        //rellenar el titulo
-        
-        //rellenar la descriocion
-        
-        //rellenar el creador
-        
-        //proposal.voters no hay que hacer nada en creacion
-        //rellenar el deadline - 30 minutos
-        
-        //inicializar el executed
-        
-        //iterar sobre el array de opciones para rellenar los mappings. Pista usar _options.length
-        for(uint8 i = 0; i < _options.length; i++){
-            //inicializar las opciones
-            
-            //inicializar los votos
-            
+        uint256 proposalId = current();
+
+        MultipleChoiceProposal storage proposal = proposals[proposalId];
+        proposal.id = proposalId;
+        proposal.title = _title;
+        proposal.description = _description;
+        proposal.creator = msg.sender;
+        proposal.deadline = block.timestamp + 30 minutes;
+        proposal.executed = false;
+        proposal.optionsNumber = uint8(_options.length);
+
+        for (uint8 i = 0; i < _options.length; i++) {
+            proposal.optionsText[i] = _options[i];
+            proposal.optionsVotes[i] = 0;
         }
-        //inicializar el numero de opciones de la propuesta
         
-        //emitir evento ProposalCreated
+        increment();
+        emit ProposalCreated(proposalId, msg.sender);
         
     }
 
@@ -143,20 +134,21 @@ contract KeepCodingDAO is Proposal, Counter {
      */
     function getProposalInfo(uint256 _proposalId) doesProposalExist(_proposalId) public view returns(MultipleChoiceProposalInfo memory){
         //obtener la propuesta cuyo id es el parametro recibido
-        
+        MultipleChoiceProposal storage proposal = proposals[_proposalId];
+
         //inicializar el objeto MultipleChoiceProposalInfo. Pista no es necesario usar storage, mejor usar memory
         MultipleChoiceProposalInfo memory proposalInfo;
+
         //inicializar el array optionsText dentro del struct
-        
+        proposalInfo.optionsText = new string[](proposal.optionsNumber);
+
         //inicializar el array optionsVotes dentro del struct
-        
+        proposalInfo.optionsVotes = new uint256[](proposal.optionsNumber);
 
         //iterar sobre las opciones. Pista usar el campo optionsNumber como tope del bucle
         for(uint8 i = 0; i < proposals[_proposalId].optionsNumber; i++){
-            //coger el optionsText
-        
-            //coger el optionsVotes
-        
+            proposalInfo.optionsText[i] = proposal.optionsText[i];
+            proposalInfo.optionsVotes[i] = proposal.optionsVotes[i];
         }
         //devolver el objeto MultipleChoiceProposalInfo
         return proposalInfo;
@@ -169,22 +161,19 @@ contract KeepCodingDAO is Proposal, Counter {
         Recibe el id de la propuesta y el codigo de la opcion.
         La propuesta debe existir y estar activa.
      */
-    function voteProposal(uint256 _proposalId, uint8 _optionCode) public doesProposalExist(_proposalId) isProposalActive(_proposalId){
-        //sumar el voto a su opcion
-        
-        //incluir el address como que ya voto
-        
-        //emitir el evento ProposalVoted
-        
-    } 
+    function voteProposal(uint256 _proposalId, uint8 _optionCode) public doesProposalExist(_proposalId) isProposalActive(_proposalId) {
+        proposals[_proposalId].optionsVotes[_optionCode]++;
+        proposals[_proposalId].voters[msg.sender] = true;
+        emit ProposalVoted(_proposalId, msg.sender);
+    }
 
     /**
         Comrpueba si una address concreta ha votado en una propuesta concreta.
         Recibe el id de la propuesta y el address que va a ser comprobado.
         La propuesta debe existir.
      */
-    function hasAddressVoted(uint256 _proposalId, address _address) public view doesProposalExist(_proposalId) returns (bool){
-        
+    function hasAddressVoted(uint256 _proposalId, address _address) public view doesProposalExist(_proposalId) returns (bool) {
+        return proposals[_proposalId].voters[_address];
     }
 
     /**
@@ -193,10 +182,8 @@ contract KeepCodingDAO is Proposal, Counter {
         La propuesta debe existir y la deadline haberse superado.
      */
     function executeProposal(uint256 _proposalId) public doesProposalExist(_proposalId) canProposalBeExecuted(_proposalId){
-        //establecer la propuesta como ejecutada
-        
-        //emitir el evento ProposalExecuted
-        
+        proposals[_proposalId].executed = true;
+        emit ProposalExecuted(_proposalId);     
     }       
 
 }
